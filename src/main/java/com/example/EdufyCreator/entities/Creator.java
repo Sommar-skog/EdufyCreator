1daf01e09bb97ef1456bb49dd88c6d653df9cbe5
Sommar-skog <amandaaronsson@hotmail.com>
2025-10-28 15:35:39 +0100

    added attributes for Creator

--- src/main/java/com/example/EdufyCreator/entities/Creator.java
+++ src/main/java/com/example/EdufyCreator/entities/Creator.java
@@ -1,4 +1,43 @@
 package com.example.EdufyCreator.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//ED-147-AA
+@Entity
+@Table(name = "creator")
 public class Creator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sub", unique = true, nullable = false)
+    private String sub;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    private List<VideoClip> videoClips = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    private List<VideoPlaylist> videoPlaylists = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    private List<Song> songs = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    private List<Album> albums = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    private List<PodcastEpisode> podcastEpisodes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    private List<PodcastSeason> podcastSeasons = new ArrayList<>();
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
 }
