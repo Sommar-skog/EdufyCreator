e9392f6fa4734d755d78eeafea57ae480d09887a
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-05 13:19:35 +0100

    Switched String mediaType to Enum MediaType in endpoint-chain.

--- src/main/java/com/example/EdufyCreator/controllers/ClientController.java
+++ src/main/java/com/example/EdufyCreator/controllers/ClientController.java
@@ -1,6 +1,7 @@
 package com.example.EdufyCreator.controllers;
 
 import com.example.EdufyCreator.models.dtos.CreatorResponseDTO;
+import com.example.EdufyCreator.models.enums.MediaType;
 import com.example.EdufyCreator.services.CreatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,7 @@ public class ClientController {
 
     //ED-146-AA //ED-257-AA
     @GetMapping("/creators-mediaid")
-    public ResponseEntity<List<CreatorResponseDTO>> getCreatorsByMediaId(@RequestParam("mediaType") String mediaType, @RequestParam("id") Long id) {
+    public ResponseEntity<List<CreatorResponseDTO>> getCreatorsByMediaId(@RequestParam("mediaType") MediaType mediaType, @RequestParam("id") Long id) {
         List<CreatorResponseDTO> creators = creatorService.getCreatorsByMediaId(mediaType, id);
         return ResponseEntity.ok(creators);
     }
